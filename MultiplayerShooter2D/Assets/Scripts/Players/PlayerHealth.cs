452f4973dbf90f47fd40e811038dc1124fc93d12
Corey Cooley <corey.a.cooley@gmail.com>
2019-11-28 22:42:28 -0500

    Player Death and Revive

--- MultiplayerShooter2D/Assets/Scripts/Players/PlayerHealth.cs
+++ MultiplayerShooter2D/Assets/Scripts/Players/PlayerHealth.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 using UnityEngine.UI;
 using Photon.Pun;
 
-public class PlayerHealth : MonoBehaviour
+public class PlayerHealth : MonoBehaviourPun
 {
 
     public Image healthFill;
+    public float currentHealth;
+    public float maxHealth = 1.0f;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Collider2D col;
+
+    private PhotonView photonVw;
+
+    void Awake() {
+        currentHealth = maxHealth;
+        photonVw = GetComponent<PhotonView>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        col = GetComponent<Collider2D>();
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -25,5 +40,33 @@ public class PlayerHealth : MonoBehaviour
     public void UpdateHealth(float damage)
     {
         healthFill.fillAmount -= damage;
+        currentHealth = healthFill.fillAmount;
+        CheckHealth();
+    }
+
+    public void CheckHealth()
+    {
+        if(photonView.IsMine && currentHealth <= 0)
+        {
+            photonVw.RPC("Death", RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+    public void Death()
+    {
+        sr.enabled = false;
+        col.enabled = false;
+        rb.velocity = Vector2.zero;
+        rb.gravityScale = 0;
+    }
+
+    [PunRPC]
+    public void Revive()
+    {
+        sr.enabled = true;
+        col.enabled = true;
+        rb.velocity = Vector2.zero;
+        rb.gravityScale = 1;
     }
 }
